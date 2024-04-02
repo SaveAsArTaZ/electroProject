29d8a69a1003f056f7e9998f14379d33d692e9d8
h.rangraz <h.rangraz@mohaymen.ir>
2024-04-02 15:41:08 +0330

    refactor shop-items

--- frontEnd/src/app/components/home-page/components/shop-items/shop-items.component.ts
+++ frontEnd/src/app/components/home-page/components/shop-items/shop-items.component.ts
@@ -17,26 +17,32 @@ export class ShopItemsComponent {
     {
       path: '../../../assets/svgs/iphone.svg',
       title: 'Mobile Phones',
+      description:
+        'mobile telephone, portable device for connecting to a telecommunications network in order to transmit and receive voice, video, or other data',
     },
     {
       path: '../../../assets/svgs/tv.svg',
       title: 'Smart TV',
+      description:
+        'In short, a smart TV is an internet enabled TV which connects to your favourite streaming platforms for endless enjoyment of your favourite content.',
     },
     {
       path: '../../../assets/svgs/watch.svg',
       title: 'Smart Watch',
+      description:
+        'A smartwatch is a portable device worn on the wrist that supports apps and acts as an extension of your mobile phone in some cases.',
     },
     {
       path: '../../../assets/svgs/laptop.svg',
       title: 'Laptops',
-    },
-    {
-      path: '../../../assets/svgs/laptop.svg',
-      title: 'Laptops',
+      description:
+        'A laptop is a personal computer that can be easily moved and used in a variety of locations. ',
     },
     {
       path: '../../../assets/svgs/drone.svg',
       title: 'Drones',
+      description:
+        "Drones are small or medium-sized unmanned aerial vehicles (UAVs). They're unique in that they can drive remotely and autonomously, and they're capable of maintaining a controlled, sustained level of flight",
     },
   ];
 }
