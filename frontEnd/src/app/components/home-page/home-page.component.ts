378cf84aa6964d5c60476eefd0059cb8cdd2f48e
h.rangraz <h.rangraz@mohaymen.ir>
2024-04-02 13:32:20 +0330

    implement offers

--- frontEnd/src/app/components/home-page/home-page.component.ts
+++ frontEnd/src/app/components/home-page/home-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { DescriptionComponent } from './components/description/description.component';
 import { ShopItemsComponent } from './components/shop-items/shop-items.component';
+import { OffersComponent } from './components/offers/offers.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [DescriptionComponent, ShopItemsComponent],
+  imports: [DescriptionComponent, ShopItemsComponent, OffersComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
