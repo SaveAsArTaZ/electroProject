b03d3cadd9385b64bce65e7361bfb3411a86fbd0
h.rangraz <h.rangraz@mohaymen.ir>
2024-04-02 12:59:35 +0330

    implement shop items carousel

--- frontEnd/src/app/components/home-page/home-page.component.ts
+++ frontEnd/src/app/components/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { DescriptionComponent } from './components/description/description.component';
+import { ShopItemsComponent } from './components/shop-items/shop-items.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [DescriptionComponent],
+  imports: [DescriptionComponent, ShopItemsComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
 })
